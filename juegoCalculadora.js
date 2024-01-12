4d8b159f9f1942acbd5e2c892d12132f1fdc48eb
Jubuntu1 <r.arnez@alumnos.upm.es>
2024-01-12 09:42:12 +0100

    juego

--- juegoCalculadora.js
+++ juegoCalculadora.js
@@ -1,41 +1,87 @@
-const input =require("prompt-sync")({sigint: true});
+const input = require("prompt-sync")({ sigint: true });
 
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+    return Math.floor(Math.random() * (max+1));
+}
 
-function suma(a,b) {
+function suma(a, b) {
     console.log(a + "+" + b)
+    let res = input("cuanto es?")
+    return(Number(res) ===(a + b))
 }
-function resta(a,b) {
+function resta(a, b) {
     console.log(a + "-" + b)
+    let res = input("cuanto es?")
+    return (Number(res)  === (a - b))
 }
-function multiplicacion(a,b) {
+function multiplicacion(a, b) {
     console.log(a + "*" + b)
+    let res = input("cuanto es?")
+    return (Number(res)  === (a * b))
 }
 
-let operacion=0;
-let opA=0;
-let opB=0;
+let operacion = 0;
+let opA = 0;
+let opB = 0;
+let tiempo=5000;
+let t=0;
 
-function jueguito(){
-for(let i;i<100;i++){
-    operacion=getRandomInt(2);
-    opA=getRandomInt(10)
-    opB=getRandomInt(10)
-    switch (operacion) {
-        case 0:
-            suma(opA,opB)
-            break;
-        case 0:
-            resta(opA,opB)
-            break;
-        case 0:
-            multiplicacion(opA,opB)
-            break;
-        default:
-            break;
+function temp(tiempo){
+    t=setTimeout(function() {
+        console.log('¡Tiempo terminado!');
+      }, tiempo);
+}
+
+
+
+function jueguito() {
+    let puntos=0
+    let fin=false
+    for (let i=0; i < 10 && !fin; i++) {
+        operacion = getRandomInt(2);
+        opA = getRandomInt(10)
+        opB = getRandomInt(10)
+        switch (operacion) {
+            case 0:
+               temp(tiempo)
+                if(suma(opA,opB)){
+                    puntos++
+                    console.log("bien")
+                }
+                else{
+                    console.log("mal")
+                    fin=true
+                }
+                break;
+            case 1:
+                temp(tiempo)
+                if(resta(opA,opB)){
+                    puntos++
+                    console.log("bien")
+                }
+                else{
+                    console.log("mal")
+                    fin=true
+                }
+                break;
+            case 2:
+                temp(tiempo)
+                if(multiplicacion(opA,opB)){
+                    puntos++
+                    console.log("bien")
+                }
+                else{
+                    console.log("mal")
+                    fin=true
+                }
+                break;
+            default:
+                break;
+        }
+        clearTimeout(t)
     }
+    console.log("tus puntos son: "+puntos)
 }
-}
+
+jueguito();
---
+++ pruebas.js
@@ -0,0 +1,12 @@
+
+  function iniciarTemporizador() {
+    // Establecer el tiempo en milisegundos (por ejemplo, 5000 para 5 segundos)
+    var tiempo = 5000;
+
+    // Llamar a la función después del tiempo especificado
+    setTimeout(function() {
+      alert('¡Tiempo terminado!');
+    }, tiempo);
+  }
+
+
